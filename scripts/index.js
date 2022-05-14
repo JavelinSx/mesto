2d432b6e098058e4ccf3cc6103c9f27a57250a28
JavelinSx <zuev499@gmail.com>
2022-05-14 09:40:36 +0300

    repair warning

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,12 @@ const buttonEdit = document.querySelector('.profile__edit-button');
 const popupClose = document.querySelector('.popup__close-button');
 const popupSubmit = document.querySelector('.popup__submit');
 const popup = document.querySelector('.popup');
+const formElement = document.querySelector('form[name=popup__form]')
 
-let photoLike = document.querySelector('.photo__item-like');
-let profileNameInput = document.querySelector('.popup__name');
-let profileActivityInput = document.querySelector('.popup__activity');
 let profileName = document.querySelector('.profile__name');
 let profileActivity = document.querySelector('.profile__activity');
+let profileNameInput = document.querySelector('input[name=profile-name]');
+let profileActivityInput = document.querySelector('input[name=profile-activity]');
 
 
 
@@ -15,7 +15,6 @@ let profileActivity = document.querySelector('.profile__activity');
 
 buttonEdit.addEventListener('click', () => {
     
-    popup.classList.remove('popup_close');
     popup.classList.add('popup_open');
     profileNameInput.value = profileName.textContent;
     profileActivityInput.value = profileActivity.textContent;
@@ -25,7 +24,6 @@ buttonEdit.addEventListener('click', () => {
 popupClose.addEventListener('click', () => {
 
     popup.classList.remove('popup_open');
-    popup.classList.add('popup_close');
 
 })
 
@@ -35,12 +33,7 @@ function formSubmitHandler (evt) {
     profileName.textContent = profileNameInput.value;
     profileActivity.textContent = profileActivityInput.value;
     popup.classList.remove('popup_open');
-    popup.classList.add('popup_close');
 }
 
-popupSubmit.addEventListener('click', formSubmitHandler)
+formElement.addEventListener('submit', formSubmitHandler)
 
-
-photoLike.addEventListener('click', () => {
-    photoLike.classList.add('photo__item-like_active')
-})
