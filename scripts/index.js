3e16ed5784b2a0b047150e0f607f9d02126d50f2
JavelinSx <zuev499@gmail.com>
2022-05-27 13:05:19 +0300

    repair

--- scripts/index.js
+++ scripts/index.js
@@ -7,10 +7,10 @@ const popupEditProfile = document.querySelector('.popup_edit-profile');
 const buttonEdit = document.querySelector('.profile__edit-button');
 const formEdit= document.querySelector('form[name=form-edit]');
 const formPhotoElement = document.querySelector('form[name=form-add-photo]');
-let profileName = document.querySelector('.profile__name');
-let profileActivity = document.querySelector('.profile__activity');
-let profileNameInput = document.querySelector('input[name=profile-name]');
-let profileActivityInput = document.querySelector('input[name=profile-activity]');
+const profileName = document.querySelector('.profile__name');
+const profileActivity = document.querySelector('.profile__activity');
+const profileNameInput = document.querySelector('input[name=profile-name]');
+const profileActivityInput = document.querySelector('input[name=profile-activity]');
 
 // popup Add photo
 const popupCloseAddPhoto = document.querySelector('.popup__close-add-photo');
@@ -18,8 +18,8 @@ const buttonAddPhoto = document.querySelector('.profile__add-photo-button');
 const popupAddPhoto = document.querySelector('.popup_add-photo');
 const ulPhotoGrid = document.querySelector('.photo__grid');
 const itemTemplate = document.querySelector('#photo__item-template').content;
-let photoNameInput = document.querySelector('input[name=photo-name]');
-let photoLinkInput = document.querySelector('input[name=photo-link]');
+const photoNameInput = document.querySelector('input[name=photo-name]');
+const photoLinkInput = document.querySelector('input[name=photo-link]');
 
 // popup Open photo
 const popupClosePhoto = document.querySelector('.popup__close-photo');
@@ -30,32 +30,7 @@ const photoTitle = document.querySelector('.photo__item-title');
 
 // work of photo
 
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
+
 function addAllPhoto(array){
     for (item of array){
         addOnePhoto(item.name, item.link)
@@ -85,6 +60,7 @@ function addOnePhoto(name, link){
       let photoSrc = e.target.src;
       popupTitlePhoto.textContent = photoAlt;
       popupPhoto.src = photoSrc;
+      popupPhoto.alt = e.target.alt;
     })
     ulPhotoGrid.insertBefore(photoItemClone,ulPhotoGrid.firstChild)
   }
