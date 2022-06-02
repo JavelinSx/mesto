5daf1a0e70e3d59795f38a06dbc5056e6fbe175a
JavelinSx <zuev499@gmail.com>
2022-06-02 20:55:06 +0300

    start validation

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,101 @@ const popupClose = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 
 // popup Edit profile
+
 const popupCloseEditProfile = document.querySelector('.popup__close-edit-profile');
 const popupEditProfile = document.querySelector('.popup_edit-profile');
 const buttonEdit = document.querySelector('.profile__edit-button');
-const formEdit= document.querySelector('form[name=form-edit]');
-const formPhotoElement = document.querySelector('form[name=form-add-photo]');
 const profileName = document.querySelector('.profile__name');
 const profileActivity = document.querySelector('.profile__activity');
-const profileNameInput = document.querySelector('input[name=profile-name]');
-const profileActivityInput = document.querySelector('input[name=profile-activity]');
+
 
 // popup Add photo
+
 const popupCloseAddPhoto = document.querySelector('.popup__close-add-photo');
 const buttonAddPhoto = document.querySelector('.profile__add-photo-button');
 const popupAddPhoto = document.querySelector('.popup_add-photo');
 const ulPhotoGrid = document.querySelector('.photo__grid');
 const itemTemplate = document.querySelector('#photo__item-template').content;
-const photoNameInput = document.querySelector('input[name=photo-name]');
-const photoLinkInput = document.querySelector('input[name=photo-link]');
+
 
 // popup Open photo
+
 const popupClosePhoto = document.querySelector('.popup__close-photo');
 const popupOpenPhoto = document.querySelector('.popup_photo-open');
 const popupTitlePhoto = document.querySelector('.popup__photo-title');
 const popupPhoto = document.querySelector('.popup__photo-wide');
 const photoTitle = document.querySelector('.photo__item-title');
+
+//-----------------
+
+//forms =>
+
+const formEdit= document.querySelector('form[name=form-edit]');
+const formPhotoElement = document.querySelector('form[name=form-add-photo]');
+
+//----------------<=
+
+//form Input =>
+
+const profileNameInput = formEdit.querySelector('input[name=profile-name]');
+const profileActivityInput = formEdit.querySelector('input[name=profile-activity]');
+const photoNameInput = document.querySelector('input[name=photo-name]');
+const photoLinkInput = document.querySelector('input[name=photo-link]');
+
+//---------------<=
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  // Находим элемент ошибки внутри самой функции
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  // Остальной код такой же
+  inputElement.classList.add('popup__input_invalid');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  // Находим элемент ошибки
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  // Остальной код такой же
+  inputElement.classList.remove('popup__input_invalid');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+const validForm = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+
+    hideInputError(formElement, inputElement);
+  }
+}; 
+
+const createEventListener = (formElement) => {
+  
+  const allInput = Array.from(formElement.querySelectorAll('.popup__input'))
+  console.log(allInput)
+  allInput.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      validForm(formElement, inputElement)
+
+    })
+  })
+}
+
+const validationForm = () => {
+  const allForm = Array.from(document.forms) 
+  allForm.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    })
+    createEventListener(formElement)
+  }) 
+}
+validationForm();
+
+
 
 // work of photo
 
